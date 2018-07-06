d645472f1ebbbe04e1197f14187076e02622613b
Brian Haller <brianhaller@Brians-MacBook-Pro-2.local>
2018-07-05 20:53:46 -0600

    commit

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 
 app.use(express.static("public"));
-app.get("*", (req, res) =>
+app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "../public/index.html"))
 );
 
